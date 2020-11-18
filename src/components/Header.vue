763d96ed40703e1f95aff84656a71039d7b70819
Liam <liamcavens@googlemail.com>
2020-11-18 16:37:05 +0000

    New Site

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="header">
+    <img
+      class="header-img"
+      src="../assets/images/LiamCartoon.jpg"
+      alt="LCImage"
+    />
+    <div class="header-titles">
+      <h2>Liam Cavens</h2>
+      <p>Software developer</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#header {
+  height: 100px;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  color: #c7c7c7;
+  background-color: #505050;
+}
+
+.header-img {
+  height: 80px;
+  border-radius: 50%;
+  margin: 10px;
+}
+
+.header-titles {
+  display: flex;
+  flex-direction: column;
+}
+
+.header-titles h2,
+.header-titles p {
+  margin: 0;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="fireworks">
+    <h1 class="bungee">Liam Cavens</h1>
+    <Firework />
+  </div>
+</template>
+
+<script>
+import Firework from "./Firework";
+export default {
+  name: "MainPage",
+  components: {
+    Firework,
+  },
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#fireworks {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.bungee {
+    font-family: 'Bungee';
+}
+</style>
